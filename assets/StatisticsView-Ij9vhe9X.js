8770643fc5549e0c5dfacf07defc62bc159f2d7b
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-02 19:30:18 +0200

    Use PNG icons and logo from icons folder

--- assets/StatisticsView-Ij9vhe9X.js
+++ assets/StatisticsView-Ij9vhe9X.js
@@ -1,4 +1,4 @@
-var ga=Object.defineProperty;var pa=(i,t,e)=>t in i?ga(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var w=(i,t,e)=>pa(i,typeof t!="symbol"?t+"":t,e);import{_ as ma,u as ba,o as xa,L as es,a as ya,g as _a,b as it,c as nt,d as z,F as Ze,k as is,y as ns,z as ss,f as xe,h as os,i as Qe,t as ht,n as va,l as Pt,m as se,q as wa,j as Ma,M as ka}from"./index-CvJ6G5pM.js";import{c as Sa,a as Pa,b as Ca,d as Da,e as Aa}from"./statistics-DIWNGio3.js";import"./i18n-b7qr553Q.js";import{S as rs}from"./SkeletonLoader-BX3NDgIJ.js";/*!
+var ga=Object.defineProperty;var pa=(i,t,e)=>t in i?ga(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var w=(i,t,e)=>pa(i,typeof t!="symbol"?t+"":t,e);import{_ as ma,u as ba,o as xa,L as es,a as ya,g as _a,b as it,c as nt,d as z,F as Ze,k as is,y as ns,z as ss,f as xe,h as os,i as Qe,t as ht,n as va,l as Pt,m as se,q as wa,j as Ma,M as ka}from"./index-B0aYwcov.js";import{c as Sa,a as Pa,b as Ca,d as Da,e as Aa}from"./statistics-DIWNGio3.js";import"./i18n-D9gBxyUb.js";import{S as rs}from"./SkeletonLoader-BB7z0TIj.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
