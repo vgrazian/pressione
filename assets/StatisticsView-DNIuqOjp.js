49155d729af86fccc1747f7225d1388ea6d3cf01
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-02 19:04:44 +0200

    Add date filter to reading list

--- assets/StatisticsView-DNIuqOjp.js
+++ assets/StatisticsView-DNIuqOjp.js
@@ -1,4 +1,4 @@
-var ga=Object.defineProperty;var pa=(i,t,e)=>t in i?ga(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var w=(i,t,e)=>pa(i,typeof t!="symbol"?t+"":t,e);import{_ as ma,u as ba,o as xa,L as es,a as ya,g as _a,b as it,c as nt,d as z,F as Ze,k as is,y as ns,z as ss,f as xe,h as os,i as Qe,t as ht,n as va,l as Pt,m as se,q as wa,j as Ma,M as ka}from"./index-Bo44lmjx.js";import{c as Sa,a as Pa,b as Ca,d as Da,e as Aa}from"./statistics-DIWNGio3.js";import"./i18n-BG7hln--.js";import{S as rs}from"./SkeletonLoader-BXQzETwj.js";/*!
+var ga=Object.defineProperty;var pa=(i,t,e)=>t in i?ga(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var w=(i,t,e)=>pa(i,typeof t!="symbol"?t+"":t,e);import{_ as ma,u as ba,o as xa,L as es,a as ya,g as _a,b as it,c as nt,d as z,F as Ze,k as is,y as ns,z as ss,f as xe,h as os,i as Qe,t as ht,n as va,l as Pt,m as se,q as wa,j as Ma,M as ka}from"./index-Bteh4jQ0.js";import{c as Sa,a as Pa,b as Ca,d as Da,e as Aa}from"./statistics-DIWNGio3.js";import"./i18n-Bh3lMxFj.js";import{S as rs}from"./SkeletonLoader-Bq2B77aT.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
