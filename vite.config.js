4f395f3c03b49b378b66d3f13f13ce87c529daef
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-02 13:45:17 +0200

    deploy: configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 
 export default defineConfig({
+    base: '/pressione/',
     plugins: [
         vue(),
         VitePWA({
