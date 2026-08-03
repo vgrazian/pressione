3b0921da49ba3be483afb540caa9c5eb0cdf6dff
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-03 18:17:05 +0200

    fix: use direct REST update for email (no RPC)

--- assets/version-9roO8Sg7.js
+++
@@ -1,1 +0,0 @@
-const s="1.1.0",a="2026-08-03T16:12:07.584Z",o="449ebe7f";export{s as A,o as B,a};
---
+++ assets/version-GqhPwUJ7.js
@@ -0,0 +1,1 @@
+const s="1.1.0",a="2026-08-03T16:16:53.165Z",o="449ebe7f";export{s as A,o as B,a};
