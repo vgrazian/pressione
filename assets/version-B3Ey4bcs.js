51af3c521827aeaa6db7a2b7c873d9b809198737
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-04 15:52:21 +0200

    fix: solo il bottone cliccato mostra 'Preparo PDF', non tutti

---
+++ assets/version-B3Ey4bcs.js
@@ -0,0 +1,1 @@
+const s="1.1.0",a="2026-08-04T13:52:17.196Z",c="bbc37a4e";export{s as A,c as B,a};
--- assets/version-cOokuYxz.js
+++
@@ -1,1 +0,0 @@
-const s="1.1.0",a="2026-08-04T13:50:17.386Z",c="bbc37a4e";export{s as A,c as B,a};
