2906f62a180db82723f003554c8204cb365831da
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-05 15:13:46 +0200

    deploy: v1.2.0

--- assets/version-BIx2rWfD.js
+++
@@ -1,1 +0,0 @@
-const s="1.2.0",a="2026-08-05T13:07:42.548Z",o="577eadb6";export{s as A,o as B,a};
---
+++ assets/version-DH9gfx83.js
@@ -0,0 +1,1 @@
+const s="1.2.0",a="2026-08-05T13:13:41.247Z",c="82f79c9e";export{s as A,c as B,a};
