323a125371bcd656552d73d8cfa0e75ca003e247
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-05 14:42:56 +0200

    deploy: v1.2.0

--- assets/version-BMBLcqJ4.js
+++
@@ -1,1 +0,0 @@
-const s="1.2.0",a="2026-08-05T12:37:24.345Z",o="f3f0891b";export{s as A,o as B,a};
---
+++ assets/version-CgkZh19I.js
@@ -0,0 +1,1 @@
+const s="1.2.0",a="2026-08-05T12:42:52.306Z",o="2df67f2e";export{s as A,o as B,a};
