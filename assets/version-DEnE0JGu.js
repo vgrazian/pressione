276ce65c6489c0396e3f58a15e8f0d36f49a8437
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-05 20:04:40 +0200

    deploy: v1.2.0

---
+++ assets/version-DEnE0JGu.js
@@ -0,0 +1,1 @@
+const s="1.2.0",a="2026-08-05T18:04:35.272Z",c="0adc3054";export{s as A,c as B,a};
--- assets/version-DVS4UyFg.js
+++
@@ -1,1 +0,0 @@
-const s="1.2.0",a="2026-08-05T16:46:20.192Z",c="0adc3054";export{s as A,c as B,a};
