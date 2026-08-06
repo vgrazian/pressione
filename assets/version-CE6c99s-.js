7176f5ca11eb383e419f4c5af58ea081b381f5aa
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-06 21:16:58 +0200

    deploy: v1.2.0

--- assets/version-CE6c99s-.js
+++
@@ -1,1 +0,0 @@
-const a="1.2.0",s="2026-08-06T19:08:46.677Z",o="4616ea3a";export{a as A,o as B,s as a};
---
+++ assets/version-MROjHU0g.js
@@ -0,0 +1,1 @@
+const a="1.2.0",s="2026-08-06T19:16:53.555Z",o="4616ea3a";export{a as A,o as B,s as a};
