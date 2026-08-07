623ec56cbad20b0b2c3b434e2390aec7aeb66264
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-07 19:28:27 +0200

    deploy: v1.2.1

--- assets/version-Bj__Nd51.js
+++
@@ -1,1 +0,0 @@
-const s="1.2.1",a="2026-08-07T17:17:14.086Z",o="0e5065fe";export{s as A,o as B,a};
---
+++ assets/version-DXNzdCu0.js
@@ -0,0 +1,1 @@
+const s="1.2.1",a="2026-08-07T17:28:22.232Z",o="f92a8efe";export{s as A,o as B,a};
