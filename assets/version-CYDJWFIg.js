b91879ede664120fdade19c6f3186182d2b7d181
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-08 13:19:33 +0200

    deploy: v1.2.1

--- assets/version-CYDJWFIg.js
+++
@@ -1,1 +0,0 @@
-const s="1.2.1",a="2026-08-08T11:12:11.062Z",o="f52ffd94";export{s as A,o as B,a};
---
+++ assets/version-dNlXYP_k.js
@@ -0,0 +1,1 @@
+const s="1.2.1",c="2026-08-08T11:19:28.016Z",a="c356cf61";export{s as A,a as B,c as a};
