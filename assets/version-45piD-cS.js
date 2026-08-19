8106bf2209e58dc3cf6a75767e859baf612d2768
Valerio Graziani <valerio.graziani@it.ibm.com>
2026-08-19 14:05:37 +0200

    deploy: v1.2.1

--- assets/version-45piD-cS.js
+++
@@ -1,1 +0,0 @@
-const a="1.2.1",s="2026-08-13T17:31:20.655Z",c="8f8ada9c";export{a as A,c as B,s as a};
---
+++ assets/version-BC9uCLcD.js
@@ -0,0 +1,1 @@
+const s="1.2.1",a="2026-08-19T12:05:31.951Z",c="2b2b827c";export{s as A,c as B,a};
